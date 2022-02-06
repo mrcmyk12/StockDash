ac4b7d21101adc8bfabd830071f3dc98eeff2e53
Philip Sanders <mrcmyk12@gmail.com>
2022-02-06 09:41:19 -0800

    Created regex for / for date to enter into stock shark api

--- src/components/App.js
+++ src/components/App.js
@@ -22,21 +22,43 @@ class App extends React.Component {
 	};
 
 	componentDidMount() {
+		let day = new Date();
+		let result = day.toLocaleDateString("en-GB", {
+			year: "numeric",
+			month: "2-digit",
+			day: "2-digit"
+		});
+		// let month = result.getMonth() + 1;
+		// let year = result.getFullYear();
+		// let date = result.getDate();
+
+		console.log(result, result.replace(/\//gi, "-"));
+
 		axios
 			.get("https://finnhub.io/api/v1/news?category=general", {
 				params: {
 					token: "c7vtq6qad3i8n3bh6ph0",
-                    minId: 10
+					minId: 10
 				}
 			})
 			.then((response) => {
-                let array = [];
+				let array = [];
 
-                for(let i = 0; i < 10; i++){
-                    array.push(response.data[i])
-                }
-                this.setState({ news: array})
-			})
+				for (let i = 0; i < 10; i++) {
+					array.push(response.data[i]);
+				}
+				this.setState({ news: array });
+			});
+
+		axios.get("https://stock-shark.com/api/v1/getHistoricalPrice", {
+			params: {
+				ticker: "DOW",
+				token: "hqhgglrpsjs",
+				startDate: "",
+				endDate: "",
+				periodType: "daily"
+			}
+		});
 	}
 
 	render() {
@@ -44,7 +66,7 @@ class App extends React.Component {
 			<div>
 				<SearchBar onSubmit={this.onSearchSubmit} />
 				<StockInfo stock={this.state.stock} />
-                <News news={this.state.news} />
+				<News news={this.state.news} />
 			</div>
 		);
 	}
