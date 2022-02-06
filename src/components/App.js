0023cfd36c4761354f9a07874f90edafbde21406
Philip Sanders <mrcmyk12@gmail.com>
2022-02-06 11:14:24 -0800

    updated date logic for api requests

--- src/components/App.js
+++ src/components/App.js
@@ -23,39 +23,53 @@ class App extends React.Component {
 
 	componentDidMount() {
 		let day = new Date();
-		let result = day.toLocaleDateString("en-US", {
+        let stringDay = day.toString();
+
+        console.log(day)
+
+        if(stringDay.includes('Sat')){
+            day.setDate(day.getDate() - 1);
+            console.log(day)
+        }
+
+        if(stringDay.includes('Sun')){
+            day.setDate(day.getDate() - 2)
+            console.log(day)
+        }
+
+        let result = day.toLocaleDateString("en-US", {
 			year: "numeric",
 			month: "2-digit",
 			day: "2-digit",
             formatMatcher: "basic"
 		});
 
-		console.log(result, result.replace(/\//gi, "-"));
+		console.log(result, result.replace(/\//gi, "-").toString());
 
-		axios
-			.get("https://finnhub.io/api/v1/news?category=general", {
-				params: {
-					token: "c7vtq6qad3i8n3bh6ph0",
-					minId: 10
-				}
-			})
-			.then((response) => {
-				let array = [];
 
-				for (let i = 0; i < 10; i++) {
-					array.push(response.data[i]);
-				}
-				this.setState({ news: array });
-			});
+
+		// axios
+		// 	.get("https://finnhub.io/api/v1/news?category=general", {
+		// 		params: {
+		// 			token: "c7vtq6qad3i8n3bh6ph0",
+		// 			minId: 10
+		// 		}
+		// 	})
+		// 	.then((response) => {
+		// 		let array = [];
+
+		// 		for (let i = 0; i < 10; i++) {
+		// 			array.push(response.data[i]);
+		// 		}
+		// 		this.setState({ news: array });
+		// 	});
 
 		axios.get("https://stock-shark.com/api/v1/getHistoricalPrice", {
 			params: {
 				ticker: "TSLA",
 				token: "hqhgglrpsjs",
-				// startDate: result.replace(/\//gi, "-").toString(),
-				// endDate: result.replace(/\//gi, "-").toString(),
-                startDate: "02-04-2022",
-                endDate: "02-04-2022",
+				startDate: result.replace(/\//gi, "-"),
+				endDate: result.replace(/\//gi, "-"),
 				periodType: "daily"
 			}
 		}).then((response) => {
@@ -63,14 +77,14 @@ class App extends React.Component {
         })
 
 
-        axios.get('https://stock-shark.com/api/v1/getLiveQuote', {
-            params: {
-                ticker: 'TSLA',
-                token: "hqhgglrpsjs"
-            }
-        }).then((response) => {
-            console.log(response)
-        })
+        // axios.get('https://stock-shark.com/api/v1/getLiveQuote', {
+        //     params: {
+        //         ticker: 'TSLA',
+        //         token: "hqhgglrpsjs"
+        //     }
+        // }).then((response) => {
+        //     console.log(response)
+        // })
 	}
 
 	render() {
