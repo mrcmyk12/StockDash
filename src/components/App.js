9351c40817486040a1e381098ad011b7a264ccf9
Philip Sanders <mrcmyk12@gmail.com>
2022-02-07 08:50:03 -0800

    added stockprice component. updated logic

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,26 @@ import axios from "axios";
 import SearchBar from "./SearchBar";
 import StockInfo from "./StockInfo";
 import News from "./News";
+import StockPrices from "./StockPrices";
 
 class App extends React.Component {
 	state = {
+		theStocks: [],
 		stock: [],
-		news: []
+		news: [],
+		stockPrice: [],
+		userStocks: [
+			"AAPL",
+			"MSFT",
+			"AMZN",
+			"FB",
+			"GOOGL",
+			"TSLA",
+			"NVDA",
+			"BRK.B",
+			"JPM",
+			"V"
+		]
 	};
 
 	onSearchSubmit = async (term) => {
@@ -23,30 +38,28 @@ class App extends React.Component {
 
 	componentDidMount() {
 		let day = new Date();
-        let stringDay = day.toString();
+		let stringDay = day.toString();
 
-        console.log(day)
+		console.log(day);
 
-        if(stringDay.includes('Sat')){
-            day.setDate(day.getDate() - 1);
-            console.log(day)
-        }
+		if (stringDay.includes("Sat")) {
+			day.setDate(day.getDate() - 1);
+			console.log(day);
+		}
 
-        if(stringDay.includes('Sun')){
-            day.setDate(day.getDate() - 2)
-            console.log(day)
-        }
+		if (stringDay.includes("Sun")) {
+			day.setDate(day.getDate() - 2);
+			console.log(day);
+		}
 
-        let result = day.toLocaleDateString("en-US", {
+		let result = day.toLocaleDateString("en-US", {
 			year: "numeric",
 			month: "2-digit",
 			day: "2-digit",
-            formatMatcher: "basic"
+			formatMatcher: "basic"
 		});
 
 		console.log(result, result.replace(/\//gi, "-").toString());
-
-
 
 		// axios
 		// 	.get("https://finnhub.io/api/v1/news?category=general", {
@@ -64,35 +77,51 @@ class App extends React.Component {
 		// 		this.setState({ news: array });
 		// 	});
 
-		axios.get("https://stock-shark.com/api/v1/getHistoricalPrice", {
-			params: {
-				ticker: "TSLA",
-				token: "hqhgglrpsjs",
-				startDate: result.replace(/\//gi, "-"),
-				endDate: result.replace(/\//gi, "-"),
-				periodType: "daily"
-			}
-		}).then((response) => {
-            console.log(response);
-        })
+		// axios.get("https://stock-shark.com/api/v1/getHistoricalPrice", {
+		// 	params: {
+		// 		ticker: this.state.userStocks,
+		// 		token: "hqhgglrpsjs",
+		// 		startDate: result.replace(/\//gi, "-"),
+		// 		endDate: result.replace(/\//gi, "-"),
+		// 		periodType: "daily"
+		// 	}
+		// }).then((response) => {
+		//     console.log(response.data.data.candles[0]);
+		//     this.setState({ stockPrice: response.data.data.candles[0]})
+		// })
 
+		const stocks = this.state.userStocks.map((stock) => {
+			return stock;
+		});
 
-        // axios.get('https://stock-shark.com/api/v1/getLiveQuote', {
-        //     params: {
-        //         ticker: 'TSLA',
-        //         token: "hqhgglrpsjs"
-        //     }
-        // }).then((response) => {
-        //     console.log(response)
-        // })
+		console.log(stocks);
+
+		axios
+			.get("https://stock-shark.com/api/v1/getLiveQuote", {
+				params: {
+					ticker: stocks.toLocaleString(),
+					token: "hqhgglrpsjs"
+				}
+			})
+			.then((response) => {
+				this.setState({ stockPrice: response.data.data });
+				console.log(this.state.stockPrice);
+			});
 	}
 
 	render() {
+
 		return (
 			<div>
 				<SearchBar onSubmit={this.onSearchSubmit} />
 				<StockInfo stock={this.state.stock} />
 				<News news={this.state.news} />
+				<StockPrices
+					names={this.state.userStocks}
+					prices={this.state.userStocks.map((stock) => {
+						return this.state.stockPrice[`${stock}`];
+					})}
+				/>
 			</div>
 		);
 	}
